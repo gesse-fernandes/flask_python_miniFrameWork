d5744c18766ea13bc38bf0a0418d9327ce8167c4
gesse-fernandes <gesse-fm@hotmail.com>
2023-01-28 11:16:41 -0300

    db connect e list e delete

--- index.py
+++ index.py
@@ -1,10 +1,15 @@
-from flask import Flask,render_template,request,session,make_response
+from flask import Flask,render_template,request,session,make_response,redirect
 import pymysql
 app = Flask(__name__)
 app.secret_key = '123'
 db = pymysql.connect(host="localhost",user='root',password='',database='youtube')
 @app.route("/",methods=["GET",'POST'])
 def hello_flask():
+    cursor = db.cursor()
+    sql = "SELECT * FROM clientes"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    '''
     if request.method == "GET":
         
         if  request.cookies.get("usuario"):
@@ -14,16 +19,22 @@ def hello_flask():
         else:
             resp = make_response("meu web site sem cookie")
             resp.set_cookie('usuario','gesse')
-        cursor = db.cursor()
-        sql = "SELECT * FROM clientes"
-        cursor.execute(sql)
-        results = cursor.fetchall()
-        print(results[0][1])
+        
         return resp 
         #return render_template("index.html",content = ['banana','perl,','ok'])
     elif request.method == "POST":
         return request.form['name']
+    '''
+    return render_template('index.html',content = results )
 
+@app.route("/deletar",methods=['GET','POST'])
+def deletar():
+    
+    cursor = db.cursor()
+    sql = "DELETE from clientes where id = %s"
+    cursor.execute(sql,(request.args.get("id"),))
+    db.commit()
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(debug=True,use_reloader=True)
