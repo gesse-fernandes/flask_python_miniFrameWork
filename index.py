ebdae43f1f528dfc1470d4204b0871bddced6a6a
gesse-fernandes <gesse-fm@hotmail.com>
2023-01-28 10:55:51 -0300

    conexão com o banco de dados

--- index.py
+++ index.py
@@ -1,13 +1,26 @@
-from flask import Flask,render_template,request
-
+from flask import Flask,render_template,request,session,make_response
+import pymysql
 app = Flask(__name__)
-
-
+app.secret_key = '123'
+db = pymysql.connect(host="localhost",user='root',password='',database='youtube')
 @app.route("/",methods=["GET",'POST'])
 def hello_flask():
     if request.method == "GET":
+        
+        if  request.cookies.get("usuario"):
+            resp = make_response('My Web Site')
 
-        return render_template("index.html",content = ['banana','perl,','ok'])
+            
+        else:
+            resp = make_response("meu web site sem cookie")
+            resp.set_cookie('usuario','gesse')
+        cursor = db.cursor()
+        sql = "SELECT * FROM clientes"
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        print(results[0][1])
+        return resp 
+        #return render_template("index.html",content = ['banana','perl,','ok'])
     elif request.method == "POST":
         return request.form['name']
 
