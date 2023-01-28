467f6105cc6d4f24034630fa3a7a77326ded0134
gesse-fernandes <gesse-fm@hotmail.com>
2023-01-28 10:06:58 -0300

    requisoces com py flask get, post

--- index.py
+++ index.py
@@ -1,11 +1,15 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/",methods=["GET",'POST'])
 def hello_flask():
-    return render_template("index.html",content = ['banana','perl,','ok'])
+    if request.method == "GET":
+
+        return render_template("index.html",content = ['banana','perl,','ok'])
+    elif request.method == "POST":
+        return request.form['name']
 
 
 if __name__ == "__main__":
